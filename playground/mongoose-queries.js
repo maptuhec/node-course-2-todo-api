8fa9b3c17aab33e8cd56eb2967ef5ff0b9e8c459
Martin Grigorov <Air@Martin.local>
2017-11-06 22:19:47 +0200

    Adding more tests

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,36 +1,40 @@
 const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-var id = '59ff67d5861016fe386ad6ec';
-
+// var id = '57bf38394b39c93d2a557e9811';
+//
 // if (!ObjectID.isValid(id)) {
 //   console.log('ID not valid');
 // }
+
 // Todo.find({
 //   _id: id
-// }).then ((todos) => {
+// }).then((todos) => {
 //   console.log('Todos', todos);
 // });
 //
 // Todo.findOne({
 //   _id: id
-// }).then ((todo) => {
+// }).then((todo) => {
 //   console.log('Todo', todo);
 // });
 
-// Todo.findById(id).then ((todo) => {
-//   if(!todo){
-//   return  console.log('ID not found');
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
 //   }
-//   console.log('TodoById', todo);
+//   console.log('Todo By Id', todo);
 // }).catch((e) => console.log(e));
 
-
-User.findById(id).then((user) => {
-  if(!user) {
-    return console.log('ID not found');
+User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
   }
-  console.log('User', user);
-}).catch((e) => console.log('ID is incorrect'));
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -30,22 +30,23 @@ app.get('/todos', (req, res) => {
   });
 });
 
-//GET /todos/123131
-  app.get('/todos/:id',(req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
-    }
-    Todo.findById(id).then((todo) => {
-      if(!todo){
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
-      }
+    }
+
     res.send({todo});
   }).catch((e) => {
-    res.status(400).send(e)
+    res.status(400).send();
   });
-
-  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
