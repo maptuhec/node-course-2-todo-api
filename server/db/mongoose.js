c287ba6d4e052a2e7ce60d2dc80aadf6cde46860
Martin Grigorov <Air@Martin.local>
2017-11-07 22:55:59 +0200

    Add Delete todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://martin:123@ds151355.mlab.com:51355/todo-app');
-
+// mongoose.connect('mongodb://martin:123@ds151355.mlab.com:51355/todo-app');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+console.log(mongoose);
 module.exports = {mongoose};
